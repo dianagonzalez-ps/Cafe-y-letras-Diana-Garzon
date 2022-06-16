2e6c517370c9e283b828e98d354c388626de4b94
Diana Garzón <dianagarzon.ps@gmail.com>
2022-06-16 06:46:45 -0500

    Actualización de archivos
    AA

--- cafeyletras/src/index.js
+++ cafeyletras/src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Header from './header/Header';
-import Navbar from './navbar/Navbar';
-import Footer from './footer/Footer';
-import Subfooter from './Subfooter/Subfooter';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <Header/>
-  <Navbar/>
-  <Footer/>
-  <Subfooter/>
-  </React.StrictMode>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}></Route>
+</Routes>
+</BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
